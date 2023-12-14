09a5743fee1f7b0d043ecf99fb06314acbef404d
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-12-14 23:51:45 +0300

    currency converter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,52 @@
-
-// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import { useState, useEffect } from "react";
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState('EUR')
+  const [toCurrency, setToCurrency] = useState('USD')
+  const [converted, setConverted] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(function(){
+    async function convert(){
+      setIsLoading(true)
+
+      const res= await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`)
+      const data = await res.json();
+
+      setConverted(data.rates[toCurrency])
+      console.log(data)
+      setIsLoading(false)
+    }
+    if(fromCurrency === toCurrency) return setConverted(amount)
+  convert();
+  
+  },[amount, fromCurrency, toCurrency])
+
 return (
   <div>
-      <input type="text" />
-      <select>
+      <input 
+      type="text" 
+      value={amount} 
+      onChange={(e)=> setAmount(Number(e.target.value))}
+      disabled={isLoading}
+      />
+
+      <select value={fromCurrency} onChange={(e)=> setFromCurrency(e.target.value)} disabled={isLoading}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
-        <option value="TL">TL</option>
-        <option value="SAR">SAR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
       </select>
-      <select>
+
+      <select value={toCurrency} onChange={(e)=> setToCurrency(e.target.value)} disabled={isLoading}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
-        <option value="TL">TL</option>
-        <option value="SAR">SAR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      
+      <p>{converted} {toCurrency}</p>
   </div>
 );
 }
