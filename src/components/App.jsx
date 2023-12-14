afb663885e144b45e7dd881e4005c79b104823c8
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-12-14 21:41:09 +0300

    Project setup

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+function App() {
+return (
+  <div>
+      <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="TL">TL</option>
+        <option value="SAR">SAR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="TL">TL</option>
+        <option value="SAR">SAR</option>
+      </select>
+      <p>OUTPUT</p>
+  </div>
+);
+}
+
+export default App;
